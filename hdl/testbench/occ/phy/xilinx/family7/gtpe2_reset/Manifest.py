2ed80637df44224317b790bebe15706eeb66bd86
Daniel Tavares <daniel.tavares@lnls.br>
2020-05-08 10:33:56 -0300

    testbench/*/gtpe2_reset: rename testbench file and module names
    Add corresponding changes in Manifest file.

--- hdl/testbench/occ/phy/xilinx/family7/gtpe2_reset/Manifest.py
+++ hdl/testbench/occ/phy/xilinx/family7/gtpe2_reset/Manifest.py
@@ -1,11 +1,11 @@
 target = "xilinx"
 action = "simulation"
 sim_tool = "modelsim"
-top_module = "occ_gtpe2_tile_tb"
+top_module = "main"
 syn_device = "xc7a200t"
 fetchto = "../../../../../../ip_cores"
 
-files = [ "occ_gtpe2_tile_tb.v" ]
+files = [ "occ_gtpe2_reset_tb.v" ]
 
 include_dirs = [ "../../../../../../sim" ]
 
