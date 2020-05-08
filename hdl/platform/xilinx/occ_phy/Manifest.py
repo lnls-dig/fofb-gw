216ee0e5cc2a7356bccb52b577cdf0b5f2900752
Daniel Tavares <daniel.tavares@lnls.br>
2020-05-08 14:59:26 -0300

    xilinx/occ_phy/family7-gtp/Manifest.py: add new module used in simulation

--- hdl/platform/xilinx/occ_phy/Manifest.py
+++ hdl/platform/xilinx/occ_phy/Manifest.py
@@ -4,4 +4,5 @@ files = [
 if (syn_device[0:4].upper()=="XC7A"): # Family 7 GTP (Artix 7)
   files.extend(["family7-gtp/occ_gtpe2_tile.vhd",
                 "family7-gtp/gtrxreset_seq.vhd",
+                "family7-gtp/gtrxreset_seq_sim.vhd",
                 "family7-gtp/sync_block.vhd" ]);
