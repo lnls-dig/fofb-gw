377ce73760eb6091e17e1446ad1a46765d753061
Lucas Russo <lerwys@gmail.com>
2020-05-08 12:09:50 -0300

    testbench/*/gtpe2_reset: fix wrong top_module name in Manifest

--- hdl/testbench/occ_phy/xilinx/family7/gtpe2_reset/Manifest.py
+++ hdl/testbench/occ_phy/xilinx/family7/gtpe2_reset/Manifest.py
@@ -1,7 +1,7 @@
 target = "xilinx"
 action = "simulation"
 sim_tool = "modelsim"
-top_module = "main"
+top_module = "occ_gtpe2_reset_tb"
 syn_device = "xc7a200t"
 fetchto = "../../../../../ip_cores"
 
