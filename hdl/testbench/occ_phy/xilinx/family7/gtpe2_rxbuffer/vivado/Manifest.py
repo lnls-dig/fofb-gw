ab9265fbb38042a08eecb74bbd9fccc5b6d614bb
Daniel Tavares <daniel.tavares@lnls.br>
2020-05-10 11:36:50 -0300

    testbench/*/gtpe2_rxbuffer: add vivado folder for xsim simulator

---
+++ hdl/testbench/occ_phy/xilinx/family7/gtpe2_rxbuffer/vivado/Manifest.py
@@ -0,0 +1,15 @@
+target = "xilinx"
+action = "simulation"
+sim_tool = "vivado_sim"
+top_module = "main"
+syn_device = "xc7a200t"
+fetchto = "../../../../../../ip_cores"
+
+include_dirs = [ "../../../../../../sim" ]
+
+modules = {
+    "local" : [
+        "..",
+        "../../../../../../platform/xilinx/occ_phy",
+    ]
+}
