045f3807615823ae2f5469a81a7a78c248f15ba9
Daniel Tavares <daniel.tavares@lnls.br>
2020-05-13 17:48:12 -0300

    testbench/*/latency_checker.v: add RX buffer and clock correction check
    Check if RX elastic buffer has signalled underflow or overflow.
    
    Check if clock correction is being perfomed within one IDLE period.

--- hdl/testbench/occ_phy/xilinx/family7/gtpe2_rxbuffer/latency_checker.v
+++ hdl/testbench/occ_phy/xilinx/family7/gtpe2_rxbuffer/latency_checker.v
@@ -29,8 +29,12 @@ module latency_checker #
   reg [15:0] current_time;
   reg [15:0] cnt_idle = 0;
 
+  reg [31:0] cnt_clk_correction = 0;
+
   reg fail_comma = 0;
   reg fail_data = 1;
+  reg fail_buffer_limit = 0;
+  reg fail_clk_correction = 0;
 
   // Data generation
   always @(posedge usrclk_i) begin
@@ -50,7 +54,7 @@ module latency_checker #
   end
 
   // Validation
-  assign fail_o = fail_comma || fail_data;
+  assign fail_o = fail_comma || fail_data || fail_buffer_limit || fail_clk_correction;
 
   always @(posedge usrclk_i) begin
     rx_realign_o <= valid_i && rx_aligned_i == 0;
@@ -90,10 +94,43 @@ module latency_checker #
         end
       end
       cnt_blind = cnt_blind + 1;
+
+      if (rx_bufstatus_i == 3'b110) begin
+        $display("RX Elastic Buffer overflow at time %d ns.", $time);
+        fail_buffer_limit <= 1; 
+      end
+      else if  (rx_bufstatus_i == 3'b101) begin
+        $display("RX Elastic Buffer underflow at time %d ns.", $time);
+        fail_buffer_limit <= 1;
+      end
+
+      // When RX buffer reaches a CLK_COR_MIN_LAT or CLK_COR_MAX_LAT limit,
+      // the warning flag at rx_bufstatus_i must be cleared in at least
+      // g_IDLE_PERIOD cycles of user clock
+      if (cnt_clk_correction >= g_IDLE_PERIOD) begin
+        if (rx_bufstatus_i == 3'b001 || rx_bufstatus_i == 3'b010) begin
+          $display("Lack of clock correction around %d ns.", $time);
+          fail_clk_correction <= 1;
+        end
+        cnt_clk_correction <= 0;
+      end
+      else if (cnt_clk_correction == 0) begin
+        if (rx_bufstatus_i == 3'b001 || rx_bufstatus_i == 3'b010) begin
+          cnt_clk_correction <= 1;
+        end
+      end
+      else begin
+        cnt_clk_correction <= cnt_clk_correction + 1;
+        if (rx_bufstatus_i == 3'b000) begin
+          cnt_clk_correction <= 0;
+        end
+      end
     end
     else begin
       fail_comma <= 0;
       fail_data <= 1;
+      fail_buffer_limit <= 0;
+      fail_clk_correction <= 0;
       cnt_blind = 0;
       right_comma_byte = 0;
     end
